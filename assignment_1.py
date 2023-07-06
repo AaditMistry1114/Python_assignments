6ff0e85417bb010971cb1d1b05f97367fcf633a5
AaditMistry1114 <138582494+AaditMistry1114@users.noreply.github.com>
2023-07-06 20:34:55 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,7 @@
+#Aadit
+print('Enter 3 numbers for average')
+num1 = int(input())
+num2= int(input())
+num3 = int(input())
+average = ((num1 + num2 + num3)/3)
+print('The average of',num1,'+',num2,'+',num3,' is = ',average)
