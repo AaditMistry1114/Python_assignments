33a1b75be0e861e3e432a7bfaea38223bb7d9610
AaditMistry1114 <138582494+AaditMistry1114@users.noreply.github.com>
2023-07-07 20:33:04 +0530

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,124 @@
+#Aadit
+
+#Arithmetic operators 
+
+arith_oper1 = 10 ;  arith_oper2 = 20
+
+print('arith_oper1 + arith_oper2=',arith_oper1 + arith_oper2) #addition
+print('arith_oper1 - arith_oper2=',arith_oper1 - arith_oper2) #subtraction
+print('arith_oper1 * arith_oper2=',arith_oper1 * arith_oper2) #multipliction
+print('arith_oper1 / arith_oper2=',arith_oper1 / arith_oper2) #division
+print('arith_oper1 % arith_oper2=',arith_oper1 % arith_oper2) #Mod
+print('arith_oper1 //arith_oper2 =',arith_oper1 // arith_oper2) #Floor division
+print('arith_oper1 **arith_oper2 =',arith_oper1 ** arith_oper2) #exponent
+
+#Assignment Operators
+
+assign_oper = 30
+
+assign_oper += 20
+print("+=",assign_oper)
+
+assign_oper = 30
+assign_oper -= 20
+print("-=",assign_oper)
+
+assign_oper = 30
+assign_oper *= 20
+print("*=",assign_oper)
+
+assign_oper = 30
+assign_oper /= 20
+print("/=",assign_oper)
+
+assign_oper = 30
+assign_oper **= 20
+print("**=",assign_oper)
+
+assign_oper = 30
+assign_oper //= 20
+print("//=",assign_oper)
+
+assign_oper = 30
+assign_oper %= 20
+print("%=",assign_oper)
+
+#Comparison Operator
+
+comp_oper1 = 10 ; comp_oper2 = 20
+
+print('comp_oper1 > comp_oper2',comp_oper1 > comp_oper2)
+print('comp_oper1 < comp_oper2',comp_oper1 < comp_oper2)
+print('comp_oper1 >= comp_oper2',comp_oper1 >= comp_oper2)
+print('comp_oper1 <= comp_oper2',comp_oper1 <= comp_oper2)
+print('comp_oper1 == comp_oper2',comp_oper1 == comp_oper2)
+print('comp_oper1 != comp_oper2',comp_oper1 != comp_oper2)
+
+#Logical Operator 
+
+log_oper1 = True ; log_oper2 = False 
+
+print('log_oper1 and log_oper2',log_oper1 and log_oper2)
+print('log_oper1 or log_oper2',log_oper1 or log_oper2)
+print(' not log_oper1 ', not log_oper1 )
+
+#Bitwise Operator
+
+bitwise_oper1 = 5
+bitwise_oper2 = 2
+
+print('Bitwise AND',bitwise_oper1 & bitwise_oper2)
+print('Bitwise OR',bitwise_oper1 | bitwise_oper2 )
+print('Bitwise XOR',bitwise_oper1 ^ bitwise_oper2)
+print('Bitwise Right Shift',bitwise_oper1 >> bitwise_oper2)
+print('Bitwise Left Shift',bitwise_oper1 << bitwise_oper2)
+
+#Identity Operator
+
+iden_oper1 = 100
+iden_oper2 = 100
+
+print('iden_oper1 is iden_oper2',iden_oper1 is iden_oper2)
+print('iden_oper1 is not iden_oper2',iden_oper1 is not iden_oper2)
+
+#Membership Operator 
+
+member_oper = 'Aadit'
+
+print('member_oper in ','A' in member_oper)
+print('member_oper not in ','t' not in member_oper)
+
+#Output 
+# arith_oper1 + arith_oper2= 30
+# arith_oper1 - arith_oper2= -10
+# arith_oper1 * arith_oper2= 200
+# arith_oper1 / arith_oper2= 0.5
+# arith_oper1 % arith_oper2= 10
+# arith_oper1 //arith_oper2 = 0
+# arith_oper1 **arith_oper2 = 100000000000000000000
+# += 50
+# -= 10
+# *= 600
+# /= 1.5
+# **= 348678440100000000000000000000
+# //= 1
+# %= 10
+# comp_oper1 > comp_oper2 False
+# comp_oper1 < comp_oper2 True
+# comp_oper1 >= comp_oper2 False
+# comp_oper1 <= comp_oper2 True
+# comp_oper1 == comp_oper2 False
+# comp_oper1 != comp_oper2 True
+# log_oper1 and log_oper2 False
+# log_oper1 or log_oper2 True
+#  not log_oper1  False
+# Bitwise AND 0
+# Bitwise OR 7
+# Bitwise XOR 7
+# Bitwise Right Shift 1
+# Bitwise Left Shift 20
+# iden_oper1 is iden_oper2 True
+# iden_oper1 is not iden_oper2 False
+# member_oper in  True
+# member_oper not in  False
+
