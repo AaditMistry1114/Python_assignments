dea67a5ca357d2f5b9ff81c379ee1c70a960db0f
AaditMistry1114 <138582494+AaditMistry1114@users.noreply.github.com>
2023-07-10 21:45:43 +0530

    Add files via upload

---
+++ assignment_4.py
@@ -0,0 +1,103 @@
+#Aadit
+#Loop Manipulation
+#1.Break
+# for i in range(1,11): 
+#     if i == 6:
+#         break
+#     print(i)
+
+#2.Continue
+# for i in range(1,11): 
+#     if i == 6:
+#         continue
+#     print(i)
+
+#3.Pass
+# for i in range(1,6): 
+#     if i == 3:
+#         pass
+#     else:
+#         print(i)
+#     print('......')
+
+#4.For Else Loop
+# for i in range(1,11):
+#     print(i)   
+# else:
+#     print('loop ends here ')
+
+#5.While Else Loop
+i=1
+while i <= 20:
+    print(i)
+    i+=1
+else:
+    print(i,'is greater than 20')
+i+=1
+
+#output 
+#1.Break
+# 1
+# 2
+# 3
+# 4
+# 5
+
+#2.Continue
+# 1
+# 2
+# 3
+# 4
+# 5
+# 7
+# 8
+# 9
+# 10
+
+#3.Pass
+# 1
+# ......
+# 2
+# ......
+# ......
+# 4
+# ......
+# 5
+# ...... 
+
+#4.For Else Loop
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# loop ends here
+
+#5.While Else Loop
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+# 20
+# 21 is greater than 20
+
